ac45822b430719b9b961828db8dbfb36d821868e
renato3x <pereirarenato21@gmail.com>
2022-04-14 10:54:36 -0300

    rota de listagem de funcionarios

--- frontend/src/app/funcionario/funcionario-routing.module.ts
+++ frontend/src/app/funcionario/funcionario-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarFuncionarioComponent } from './pages/listar-funcionario/listar-funcionario.component';
 import { NovoFuncionarioComponent } from './pages/novo-funcionario/novo-funcionario.component';
 
 const routes: Routes = [
   {
     path: 'novo-funcionario',
     component: NovoFuncionarioComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: ListarFuncionarioComponent
   }
 ]
 
