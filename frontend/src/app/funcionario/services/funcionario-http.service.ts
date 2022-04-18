34e80a995572f3c40540371b113892313ed911f4
renato3x <pereirarenato21@gmail.com>
2022-04-18 09:47:34 -0300

    url base e interface funcionario criados

--- frontend/src/app/funcionario/services/funcionario-http.service.ts
+++ frontend/src/app/funcionario/services/funcionario-http.service.ts
@@ -4,7 +4,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class FuncionarioHttpService {
 
+  private readonly baseURL = 'http://localhost:8080/servicos/funcionario'
+
   constructor(
     private http: HttpClient
-  ) { }
+  ) {}
 }
